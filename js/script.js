7031ce532d81509a0fe8d733c782f2e645345dd2
bafermc <yuanqiwei71@gmail.com>
2023-05-27 22:22:42 +0900

    5/27 最終：チェックボックス式ライト/ダーク切り替え実装

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 // チェックボックスの取得
 const darkcheck = document.querySelector("#checkbox-darkmode");
 const btn_dark = document.getElementById('button-darkmode');
+
+//サイトを読み込んだ時：デフォルトでLight Mode
+document.body.classList.remove("dark-theme");
+document.body.classList.add("light-theme");
+btn_dark.textContent = 'Dark Mode';
  
 // チェックした時の挙動
 darkcheck.addEventListener("change", () => {
@@ -8,11 +13,11 @@ darkcheck.addEventListener("change", () => {
     // ダークモード
     document.body.classList.remove("light-theme");
     document.body.classList.add("dark-theme");
-    btn_dark.textContent = 'Dark Mode';
+    btn_dark.textContent = 'Light Mode';
   } else {
     // ライトモード
     document.body.classList.remove("dark-theme");
     document.body.classList.add("light-theme");
-    btn_dark.textContent = 'Light Mode';
+    btn_dark.textContent = 'Dark Mode';
   }
 });
