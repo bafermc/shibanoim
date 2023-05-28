184cafa3ec7c9fa96a4d5b4fadf1504cd10be9c9
bafermc <yuanqiwei71@gmail.com>
2023-05-28 18:25:11 +0900

    ロゴマーク色変更を追加

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // チェックボックスの取得
 const lightmode = document.querySelector("#on");
 const darkmode = document.querySelector("#off");
+
+document.body.classList.remove("dark-theme");
+document.body.classList.add("light-theme");
  
 // チェックした時の挙動
 darkmode.addEventListener("change", () => {
@@ -8,6 +11,7 @@ darkmode.addEventListener("change", () => {
     // ダークモード
     document.body.classList.remove("light-theme");
     document.body.classList.add("dark-theme");
+    document.getElementById("logo_image").src='./images/shibanorail_darklogo.png';
   }
 });
 
@@ -16,5 +20,6 @@ lightmode.addEventListener("change", () => {
     // ダークモード
     document.body.classList.remove("dark-theme");
     document.body.classList.add("light-theme");
+    document.getElementById("logo_image").src='./images/shibanorail_lightlogo.png';
   }
 });
