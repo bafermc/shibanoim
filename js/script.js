391ec92bbeadc906f41532889dc9805869df0617
bafermc <yuanqiwei71@gmail.com>
2023-05-28 11:30:50 +0900

    スライドボタンで実装

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
 // チェックボックスの取得
-const darkcheck = document.querySelector("#checkbox-darkmode");
-const btn_dark = document.getElementById('button-darkmode');
-
-//サイトを読み込んだ時：デフォルトでLight Mode
-document.body.classList.remove("dark-theme");
-document.body.classList.add("light-theme");
-btn_dark.textContent = 'Dark Mode';
+const lightmode = document.querySelector("#on");
+const darkmode = document.querySelector("#off");
  
 // チェックした時の挙動
-darkcheck.addEventListener("change", () => {
-  if (darkcheck.checked == true) {
+darkmode.addEventListener("change", () => {
+  if (darkmode.checked == true) {
     // ダークモード
     document.body.classList.remove("light-theme");
     document.body.classList.add("dark-theme");
-    btn_dark.textContent = 'Light Mode';
-  } else {
-    // ライトモード
+  }
+});
+
+lightmode.addEventListener("change", () => {
+  if (lightmode.checked == true) {
+    // ダークモード
     document.body.classList.remove("dark-theme");
     document.body.classList.add("light-theme");
-    btn_dark.textContent = 'Dark Mode';
   }
 });
